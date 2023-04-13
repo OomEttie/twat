3dabeec467877d58b29f7ae98352641afdf87c26
oomEttie <etienne.landman@vulcanlabs.com>
2023-04-13 16:21:32 +0200

    database config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,33 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: ['@ianvs/prettier-plugin-sort-imports', "prettier-plugin-tailwindcss"],
+  // arrowParens: 'always',
+  // printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  // semi: false,
+  // trailingComma: 'all',
+  // useTabs: false,
+  // tabWidth: 2,
+//  tailwindConfig: './packages/tailwind',
+//  importOrder: [
+//   '^(preact/(.*)$)|^(preact$)',
+//   '<THIRD_PARTY_MODULES>',
+//   '',
+//   '^@iidentifii/(.*)$',
+//   '',
+//   '^~/utils/(.*)$',
+//   '^~/components/(.*)$',
+//   '^~/styles/(.*)$',
+//   '^~/(.*)$',
+//   '^[./]',
+//  ],
+//  importOrderSeparation: false,
+//  importOrderSortSpecifiers: true,
+//  importOrderBuiltinModulesToTop: true,
+//  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+//  importOrderMergeDuplicateImports: true,
+//  importOrderCombineTypeAndValueImports: true,
 };
 
 module.exports = config;
